6f7be6c9a6b0bca7c17a8b60c65ff06bb75da90b
Nitish Pant <nitishpant@users.noreply.github.com>
2019-05-05 17:02:09 +0530

    made more efficient

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-console.log('Starting app.js');
 const fs = require('fs');
 const _ =require('lodash');
 const yargs=require('yargs');
@@ -7,26 +6,43 @@ const notes =require('./notes.js');
 
 const argv =yargs.argv;
 var command =argv._[0];
-console.log('Command: ',command);
-console.log('yargs',argv);
+
 
 if (command==='add'){
    var note= notes.addnote(argv.title,argv.body);  
    if(note) {
     console.log('note created');
-    console.log('----');
-    console.log('TITLE :'+note.title);
-    console.log('BODY :'+note.body);
+   notes.lognote(note);
    }else {
     console.log('note title already taken');
 
    }
-} else if ( command ==='list'){
-    notes.getall();
-} else if( command==='read') {
-    notes.getnote(argv.title);
-} else if (command ==='remove'){
-    notes.remove(argv.title);
+}
+ else if ( command ==='list'){
+    var allnotes=notes.getall();
+    console.log('printing '+ allnotes.length+' note(s)');
+    allnotes.forEach((note) =>notes.lognote(note));
+    
+}
+
+ else if( command==='read') {
+    var notefound=notes.getnote(argv.title);            //notes variable is requiring notes.js
+    if(notefound) {
+    console.log('note found');
+    notes.lognote(notefound);
+    }else {
+        console.log('note not found');
+    
+       }
+
+}
+ else if (command ==='remove'){
+    var noteremoved=notes.remove(argv.title);
+    var message = noteremoved ? 'note was removed successfully' : 'Note not found';
+    console.log(message);
+
+    
+
 } else {
     console.log('enter a valid command')
 }
