e9a51ff037f55e123735c43777b434253572c37b
Nitish Pant <nitishpant@users.noreply.github.com>
2019-05-05 17:00:37 +0530

    added listing,removing and reading note functionality

--- notes.js
+++ notes.js
@@ -37,21 +37,19 @@ module.exports={
 */
 
 // now moving things into functions
-console.log('starting notes.js');
+
 const fs= require('fs');
 var fetchnotes =() => {
     try{ var notessrting=fs.readFileSync('notes-data.json');
     return notes=JSON.parse(notessrting);  // adding new notes
 
     } catch (e) {
-    
     return [];
     }
 };
 
 var savenotes =(notes) => {
     fs.writeFileSync('notes-data.json',JSON.stringify(notes));
-
 };
 var addnote = (title,body) => {
     var notes = fetchnotes();
@@ -69,15 +67,30 @@ var addnote = (title,body) => {
     }
 };
 var getall =() =>{
-    console.log('Getting all notes');
+    return fetchnotes();
 
 };
 var getnote =(title) => {
-    console.log('Getting note',title);
+    var notes=fetchnotes();
+    var filterednots=notes.filter((note)=>note.title===title);
+    return filterednots[0];
 };
 var remove =(title) =>{
-    console.log('Removing note',title)
+    var notes= fetchnotes();
+    var newnotes=[]; 
+    var duplicatenotes;
+    if( duplicatenotes=notes.filter((note)=>note.title!=title)){
+        newnotes=duplicatenotes;
+    }
+    savenotes(newnotes);
+    return notes.length!=newnotes.length;
+};
+var lognote=(note)=>{
+    debugger;
+    console.log('----');
+    console.log('TITLE :'+note.title);
+    console.log('BODY :'+note.body);
 };
 module.exports={
-    addnote,getall,getnote,remove
+    addnote,getall,getnote,remove,lognote              //es6 syntax
 };
